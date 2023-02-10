716d756c6ea2a8e7ad7feaa1b29f59494ca5cfab
aldlss <ayaldlss@gmail.com>
2023-02-11 05:37:53 +0800

    fix: 删除测试代码
    刚注意到……

--- app/cmd/api/main.go
+++ app/cmd/api/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"github.com/agiledragon/gomonkey/v2"
 	"github.com/aldlss/MiniTikTok-Social-Module/app/cmd/api/handle"
 	"github.com/aldlss/MiniTikTok-Social-Module/app/cmd/api/middleware"
 	"github.com/aldlss/MiniTikTok-Social-Module/app/cmd/api/rpc"
-	"github.com/aldlss/MiniTikTok-Social-Module/app/kitex_gen/pb/auth"
 	"github.com/aldlss/MiniTikTok-Social-Module/app/pkg/constants"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
@@ -27,22 +25,6 @@ func main() {
 	relationGroup := tiktokGroup.Group("/relation")
 	relationGroup.POST("/action", handle.RelationFollowAction)
 	relationGroup.GET("/:relation/list", handle.RelationList)
-
-	patches := gomonkey.ApplyFunc(rpc.AuthRPC, func(ctx context.Context, req *auth.AuthRequest) (uint32, error) {
-		switch req.Token {
-		case "aya":
-			return 11, nil
-		case "satori":
-			return 12, nil
-		case "remilia":
-			return 13, nil
-		case "koishi":
-			return 14, nil
-		default:
-			return 0, nil
-		}
-	})
-	defer patches.Reset()
 
 	r.NoRoute(func(c context.Context, ctx *app.RequestContext) {
 		ctx.Status(constants.NotFound)
