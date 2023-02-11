9ba977333f588afd09a1fdf4d9a9afff121743bd
aldlss <ayaldlss@gmail.com>
2023-02-12 02:32:25 +0800

    chore: 在 relation 服务中增加 log 输出错误的代码
    虽然应该会在 api 服务中再输出一次，但毕竟是不同的服务，还是区分开来比较好

--- app/cmd/relation/handler.go
+++ app/cmd/relation/handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aldlss/MiniTikTok-Social-Module/app/cmd/relation/service"
 	"github.com/aldlss/MiniTikTok-Social-Module/app/kitex_gen/pb/relation"
 	"github.com/aldlss/MiniTikTok-Social-Module/app/pkg/errno"
+	log "github.com/sirupsen/logrus"
 )
 
 // RelationServiceImpl implements the last service interface defined in the IDL.
@@ -14,14 +15,19 @@ type RelationServiceImpl struct{}
 // FollowAction implements the RelationServiceImpl interface.
 func (s *RelationServiceImpl) FollowAction(ctx context.Context, req *relation.FollowActionRequest) (resp *relation.FollowActionResponse, err error) {
 	if req.Id == 0 || req.ToUserId == 0 {
-		return pack.BuildActionResp(errno.ParamErr.WriteMsg("Params can't be 0")), nil
+		err := errno.ParamErr.WriteMsg("Params can't be 0")
+		log.Error(err.Error())
+		return pack.BuildActionResp(err), nil
 	}
 	if req.Id == req.ToUserId {
-		return pack.BuildActionResp(errno.ParamErr.WriteMsg("Can't follow self")), nil
+		err := errno.ParamErr.WriteMsg("Can't follow self")
+		log.Error(err.Error())
+		return pack.BuildActionResp(err), nil
 	}
 
 	err = service.NewFollowActionService(ctx).FollowAction(req)
 	if err != nil {
+		log.Error(err.Error())
 		return pack.BuildActionResp(err), nil
 	}
 	return pack.BuildActionResp(errno.Success), nil
@@ -30,6 +36,7 @@ func (s *RelationServiceImpl) FollowAction(ctx context.Context, req *relation.Fo
 func (s *RelationServiceImpl) ListFollow(ctx context.Context, req *relation.ListFollowRequest) (resp *relation.ListFollowResponse, err error) {
 	followList, err := service.NewListFollowService(ctx).ListFollow(req)
 	if err != nil {
+		log.Error(err.Error())
 		return pack.BuildListFollowResp(nil, err), nil
 	}
 	return pack.BuildListFollowResp(followList, errno.Success), nil
@@ -38,6 +45,7 @@ func (s *RelationServiceImpl) ListFollow(ctx context.Context, req *relation.List
 func (s *RelationServiceImpl) ListFans(ctx context.Context, req *relation.ListFansRequest) (resp *relation.ListFansResponse, err error) {
 	fansList, err := service.NewListFansService(ctx).ListFans(req)
 	if err != nil {
+		log.Error(err.Error())
 		return pack.BuildListFansResp(nil, err), nil
 	}
 	return pack.BuildListFansResp(fansList, errno.Success), nil
@@ -46,6 +54,7 @@ func (s *RelationServiceImpl) ListFans(ctx context.Context, req *relation.ListFa
 func (s *RelationServiceImpl) ListFriends(ctx context.Context, req *relation.ListFriendsRequest) (resp *relation.ListFriendsResponse, err error) {
 	friendsList, err := service.NewListFriendsService(ctx).ListFriends(req)
 	if err != nil {
+		log.Error(err.Error())
 		return pack.BuildListFriendsResp(nil, err), nil
 	}
 	return pack.BuildListFriendsResp(friendsList, errno.Success), nil
